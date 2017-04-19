d1b20c2338578eafa3aaaddc68f1b9096e086064
Rafi <rafi.bogra@gmail.com>
2017-04-19 17:40:08 +0000

    initial

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+var Database = require('better-sqlite3');
+
+router.route('/tr')
+  .get(function(req, res, next) {
+    var db = new Database('data/tr1/SeradexTracker.sqlite', {});
+    var rows = db.prepare('SELECT * FROM '+req.params.table).all();
+    res.send(rows);
+  })
+  .post(function(req, res, next) {
+    var dbPath = req.body.path;
+    var table = req.body.table;
+    
+    try {
+      var db = new Database(dbPath, {});
+      var rows = db.prepare('SELECT * FROM ' + table).all();
+    } catch(error) {
+      console.log('error');
+      return next(error);
+    }
+    
+    console.log('passed');
+    res.send(rows);
+  });
+    
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var routes = require('./routes');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+
+app.use('/api', routes);
+
+
+// Hndle Error
+app.use(function(err, req, res, next) {
+  res.status(500).send({error: err});
+})
+
+var port = process.env.PORT || 3000;
+app.set('port', port);
+var server = http.createServer(app);
+
+app.listen(port, function() {
+  console.log('server running, port:'+ port);
+});
